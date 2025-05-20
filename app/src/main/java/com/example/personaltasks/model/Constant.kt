91c2623804d18c74e946a89a4db71890905bb6f9
mhenriquecode <matheucar186@gmail.com>
2025-05-20 17:16:40 -0300

    refactor : task parcelable

--- app/src/main/java/com/example/personaltasks/model/Constant.kt
+++ app/src/main/java/com/example/personaltasks/model/Constant.kt
@@ -3,4 +3,5 @@ package com.example.personaltasks.model
 object Constant {
     const val EXTRA_TASK = "EXTRA_TASK"
     const val EXTRA_VIEW_TASK = "EXTRA_VIEW_TASK"
+    const val INVALID_TASK_ID = -1
 }
--- app/src/main/java/com/example/personaltasks/model/Task.kt
+++ app/src/main/java/com/example/personaltasks/model/Task.kt
@@ -1,7 +1,13 @@
 package com.example.personaltasks.model
 
+import android.os.Parcelable
+import com.example.personaltasks.model.Constant.INVALID_TASK_ID
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
 data class Task(
-    val title: String,
-    val description: String,
-    val deadline: String
-)
+    var id: Int? = INVALID_TASK_ID,
+    var title: String = "",
+    var description: String = "",
+    var deadline: String = ""
+) : Parcelable
