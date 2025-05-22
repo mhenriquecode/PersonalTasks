588dd8cef668511f9e6f5879433a22917807996d
mhenriquecode <matheucar186@gmail.com>
2025-05-21 21:38:50 -0300

    feat: implementing TaskDAO

---
+++ app/src/main/java/com/example/personaltasks/model/TaskDAO.kt
@@ -0,0 +1,9 @@
+package com.example.personaltasks.model
+
+interface TaskDao {
+    fun createTask(task: Task): Long
+    fun retrieveTask(id: Int): Task
+    fun retrieveTasks(): MutableList<Task>
+    fun updateTask(task: Task): Int
+    fun deleteTask(id: Int): Int
+}
