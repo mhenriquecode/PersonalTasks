7d453f937f05b327a19c485744ad2fa3540417a4
mhenriquecode <matheucar186@gmail.com>
2025-06-26 18:25:34 -0300

    feat: Adding LoginActivity

---
+++ app/src/main/java/com/example/personaltasks/view/LoginActivity.kt
@@ -0,0 +1,21 @@
+package com.example.personaltasks.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.personaltasks.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
+
+class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        auth = FirebaseAuth.getInstance()
+
+    }
+
+}
