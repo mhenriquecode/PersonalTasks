4650cfc778865781ace2778ea5194d6106523690
mhenriquecode <matheucar186@gmail.com>
2025-06-27 00:41:01 -0300

    feat: adding getDeletedTasks and using all context in mainController

--- app/src/main/java/com/example/personaltasks/controller/MainController.kt
+++ app/src/main/java/com/example/personaltasks/controller/MainController.kt
@@ -1,16 +1,22 @@
 package com.example.personaltasks.controller
 
+import android.content.Context
 import com.example.personaltasks.model.Task
 import com.example.personaltasks.model.TaskDao
 import com.example.personaltasks.model.TaskSqlite
-import com.example.personaltasks.view.MainActivity
 
-class MainController(mainActivity: MainActivity) {
-    private val taskDao: TaskDao = TaskSqlite(mainActivity)
+class MainController(context: Context) {
+    private val taskDao: TaskDao = TaskSqlite(context)
 
     fun insertTask(task: Task) = taskDao.createTask(task)
     fun getTask(id: Int) = taskDao.retrieveTask(id)
     fun getAllTasks() = taskDao.retrieveTasks()
+    fun getDeletedTasks() = taskDao.retrieveDeletedTasks()
     fun updateTask(task: Task) = taskDao.updateTask(task)
-    fun removeTask(id: Int) = taskDao.deleteTask(id)
+
+    // Remove logicamente a tarefa, marcando isDeleted = true
+    fun removeTask(task: Task): Int {
+        val deletedTask = task.copy(isDeleted = true)
+        return taskDao.updateTask(deletedTask)
+    }
 }
