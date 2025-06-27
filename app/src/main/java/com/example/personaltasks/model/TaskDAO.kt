2bffea11f2df3bb26502fb6c6e976bccb6188712
mhenriquecode <matheucar186@gmail.com>
2025-06-27 00:36:40 -0300

    feat: creating retrieveDeletedTasks

--- app/src/main/java/com/example/personaltasks/model/TaskDAO.kt
+++ app/src/main/java/com/example/personaltasks/model/TaskDAO.kt
@@ -4,6 +4,7 @@ interface TaskDao {
     fun createTask(task: Task): Long
     fun retrieveTask(id: Int): Task
     fun retrieveTasks(): MutableList<Task>
+    fun retrieveDeletedTasks(): MutableList<Task>
     fun updateTask(task: Task): Int
     fun deleteTask(id: Int): Int
 }
